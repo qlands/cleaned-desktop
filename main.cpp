3fd7ecadcfa970c072dca46e05beb2efd5d41893
Carlos Quiros <cquiros@qlands.com>
2023-07-26 09:48:00 -0700

    Remove binaries

--- main.cpp
+++ main.cpp
@@ -64,9 +64,9 @@ int main(int argc, char *argv[])
     Q_INIT_RESOURCE(cleaned);
 
     QApplication app(argc, argv);
-    QCoreApplication::setApplicationName("CLEANED_20220411");
+    QCoreApplication::setApplicationName("CLEANED_20230726");
     QCoreApplication::setOrganizationName("Alliance Bioversity-CIAT");
-    QCoreApplication::setApplicationVersion("20220411");
+    QCoreApplication::setApplicationVersion("20230726");
     QCommandLineParser parser;
     parser.setApplicationDescription("Cleaned Desktop App");
     parser.addHelpOption();
