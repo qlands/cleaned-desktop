0f928820b29cb1bbbf96eb3ae6ce5aff7b2ecc16
Ramin Raeisi <rr@epen.io>
2023-07-30 19:40:42 +0200

    Update R's comparison model

--- R/compare_modesl.R
+++ R/compare_modesl.R
@@ -1,12 +1,13 @@
 args = commandArgs(trailingOnly=TRUE)
 
+comparison_args <- args[-2]
+ 
 if (length(args)<3)
 {
   stop("You need to indicate at least two model result files and a comparison result file", call.=FALSE)
 }
 
 .libPaths()
-
 
 if (!require("devtools"))
 {
@@ -15,20 +16,23 @@ if (!require("devtools"))
 
 if (!require("cleaned"))
 {
-  library("devtools")
-  devtools::install_github("ciat/cleaned@v0.3.2")
-} else if (packageVersion("cleaned") != '0.3.2') {
-  library("devtools")
-  devtools::install_github("ciat/cleaned@v0.3.2")
+  install.packages("ggplot2")
 }
 
-# library("devtools")
-# devtools::install_github("ciat/cleaned")
+if (!require("cleaned"))
+{
+  library("devtools")
+  devtools::install_github("ciat/cleaned@cleaned_v0.4.0")
+} else if (packageVersion("cleaned") != '0.3.2') {
+  library("devtools")
+  devtools::install_github("ciat/cleaned@cleaned_v0.4.0")
+}
 
 # load libraries
 library(cleaned)
 library(jsonlite)
-print(args)
-calculate_differences(args)
+library(ggplot2)
 
+do.call("calculate_differences", as.list(comparison_args))
+clean_plotting(args[1], args[2])
 
